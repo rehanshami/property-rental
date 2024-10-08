ad2d7696b6e47bd80c3c7d8973631c30caab8a2d
rehanshami <77270784+rehanshami@users.noreply.github.com>
2024-10-09 12:34:15 +1300

    feat: Create Home Properties component and render on homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
+import HomeProperties from "@/components/HomeProperties";
 const HomePage = () => {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <HomeProperties />
     </>
   );
 };
