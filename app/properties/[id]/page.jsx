fd57220f66b3ae309a0f2e3757685ee8fbc9e249
rehanshami <77270784+rehanshami@users.noreply.github.com>
2024-10-13 15:45:53 +1300

    feat: Create Property details section

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,6 +1,35 @@
-const PropertyPage = ({ params, searchParams }) => {
-  console.log("Property page component");
-  return <div>Property page {params.id}</div>;
+import PropertyDetails from "@/components/PropertyDetails";
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import PropertyHeaderImage from "@/components/PropertyHeaderImage";
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa";
+const PropertyPage = async ({ params }) => {
+  await connectDB();
+  const property = await Property.findById(params.id).lean();
+  return (
+    <>
+      <PropertyHeaderImage image={property.images[0]} />
+      <section>
+        <div className="container m-auto py-6 px-6">
+          <Link
+            href="/properties"
+            className="text-blue-500 hover:text-blue-600 flex items-center"
+          >
+            <FaArrowLeft className=" mr-2"></FaArrowLeft> Back to Properties
+          </Link>
+        </div>
+      </section>
+      <section class="bg-blue-50">
+        <div class="container m-auto py-10 px-6">
+          <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+            {/* Property Info */}
+            <PropertyDetails property={property} />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default PropertyPage;
