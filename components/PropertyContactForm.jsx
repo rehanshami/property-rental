bece7ec5d82bb3311535a82f93b23a3464b3e23d
rehanshami <77270784+rehanshami@users.noreply.github.com>
2024-10-20 21:09:22 +1300

    feat: Create useFormStatus hook for pending message status

--- components/PropertyContactForm.jsx
+++ components/PropertyContactForm.jsx
@@ -1,10 +1,10 @@
 "use client";
 import { useEffect } from "react";
-import { useFormState, useFormStatus } from "react-dom";
+import { useFormState } from "react-dom";
 import { useSession } from "next-auth/react";
 import { toast } from "react-toastify";
 import addMessage from "@/app/actions/addMessage";
-import { FaPaperPlane } from "react-icons/fa";
+import SubmitMessageButton from "./SubmitMessageButton";
 const PropertyContactForm = ({ property }) => {
   const { data: session } = useSession();
   const [state, formAction] = useFormState(addMessage, {});
@@ -97,12 +97,7 @@ const PropertyContactForm = ({ property }) => {
             ></textarea>
           </div>
           <div>
-            <button
-              className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline flex items-center justify-center"
-              type="submit"
-            >
-              <FaPaperPlane className="mr-2" /> Send Message
-            </button>
+            <SubmitMessageButton />
           </div>
         </form>
       </div>
